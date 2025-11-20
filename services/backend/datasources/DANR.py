c8869eeea7bf47c0c78eabb9f8390bd57a055e7f
Nj38 <jairamkartik38@gmail.com>
2025-11-20 15:00:17 -0500

    DANRtest
    Made DANR v2

--- services/backend/datasources/DANR.py
+++ services/backend/datasources/DANR.py
@@ -2,9 +2,11 @@ import requests
 from base2 import DataSource
 import json
 from datetime import datetime
+import pandas as pd
+import sqlite3
 
 baseURLS = {"DANR" : "https://apps.sd.gov/NR92WQMAP/api/station/", "NDMES" : ""}
-StationList = {"DANR" : ["SWLAZZZ2411A"]}
+StationList = {"DANR" : ["SWLAZZZ2411A", "CAMPPOCP01", "SD_11904"]}
 
 class DANR(DataSource):
     def __init__(self, start_date=None, format=None):
@@ -29,17 +31,30 @@ class DANR(DataSource):
                 if EpochTime<=self.cutoff:
                     count +=1
 
-        del self.data[station_index]['parameters'][:count]
 
-        print(json.dumps(self.data, indent=4))
+        del self.data[station_index]['parameters'][:count]
     
     def _push(self):
-        pass
+        conn = sqlite3.connect('mydatabase.db')
+        db = pd.DataFrame()
+        for data in self.data:
+
+            meta = [['station', key] for key in list(data['station'].keys())]
+            temp = pd.json_normalize(data=data, record_path= 'parameters', meta=meta)
+            db = pd.concat([db, temp])
+
+        db.to_sql('mydatabase.db', conn, if_exists='replace', index=False)
+
+        print(db)
+
+        conn.close()
+
     
 def main():
     test = DANR("2025-8-01 0:0:0", "%Y-%m-%d %H:%M:%S")
     test._pull()
     test._process()
+    test._push()
 
 if (__name__ == "__main__"):
     main()
